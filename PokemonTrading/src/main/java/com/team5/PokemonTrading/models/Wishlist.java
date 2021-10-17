e3f6c7eaaeac5705b1d624ca4541c3458dc732fa
DistractedBranches <84642901+DistractedBranches@users.noreply.github.com>
2021-10-17 15:00:56 -0400

    Wishlist model

---
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/models/Wishlist.java
@@ -0,0 +1,52 @@
+package com.team5.PokemonTrading.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="wishlist")
+public class Wishlist {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",insertable = false)
+    @Column(name = "userid")
+    private Integer userid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pokeid",insertable = false)
+    @Column(name = "pokeid")
+    private Integer pokeid;
+
+    public Wishlist(Integer userid, Integer pokeid) {
+        this.userid = userid;
+        this.pokeid = pokeid;
+    }
+
+    public Wishlist() {
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getPokeid() {
+        return pokeid;
+    }
+
+    public void setPokeid(Integer pokeid) {
+        this.pokeid = pokeid;
+    }
+
+    @Override
+    public String toString() {
+        return "Wishlist{" +
+                "userid=" + userid +
+                ", pokeid=" + pokeid +
+                '}';
+    }
+}
