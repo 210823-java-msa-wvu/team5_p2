3f5dcecef6bbe0a998c205d14efaaf6fd8e7429f
y888miao <89420590+y888miao@users.noreply.github.com>
2021-10-18 10:52:55 -0400

    Update FrontResource.java

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/FrontResource.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/FrontResource.java
@@ -3,6 +3,7 @@ package com.team5.PokemonTrading.resource;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team5.PokemonTrading.models.User;
+import com.team5.PokemonTrading.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
