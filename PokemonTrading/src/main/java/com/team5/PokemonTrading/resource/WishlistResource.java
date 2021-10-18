d93356e8dbec396842f0c4ba6ea9fd9f205d6d8f
DistractedBranches <damolajao@gmail.com>
2021-10-18 06:15:07 -0400

    Initial services for wishlist added
    Further specialization for user get will be modified later

---
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/WishlistResource.java
@@ -0,0 +1,4 @@
+package com.team5.PokemonTrading.resource;
+
+public class WishlistResource {
+}
--- PokemonTrading/src/main/java/com/team5/PokemonTrading/services/WishlistServices.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/services/WishlistServices.java
@@ -1,4 +1,24 @@
 package com.team5.PokemonTrading.services;
 
+import com.team5.PokemonTrading.models.Wishlist;
+import com.team5.PokemonTrading.repos.WishlistRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class WishlistServices {
+    private final WishlistRepo wishlistRepo;
+
+    @Autowired
+    public WishlistServices(WishlistRepo wishlistRepo){ this.wishlistRepo = wishlistRepo;}
+
+    public Wishlist addWishlist(Wishlist wishlist){ return wishlistRepo.save(wishlist);}
+
+    public void deleteWishlist(Integer id){ wishlistRepo.deleteById(id);}
+
+
+
 }
