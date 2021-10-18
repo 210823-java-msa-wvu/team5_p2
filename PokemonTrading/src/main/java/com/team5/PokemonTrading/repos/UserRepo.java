36e12408e64eae037ba42a9dfe028469551cb193
lopezbraca96 <lopezbraca96@gmail.com>
2021-10-18 10:17:05 -0400

    Userservice added

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/repos/UserRepo.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/repos/UserRepo.java
@@ -1,7 +1,14 @@
 package com.team5.PokemonTrading.repos;
 
+import com.team5.PokemonTrading.models.Pokemon;
 import com.team5.PokemonTrading.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User,Integer> {
+    void deleteUserById(Integer id);
+
+    Optional<User> findUserById(Integer id);
+    Optional<User> findUserByUsername(String username);
 }
