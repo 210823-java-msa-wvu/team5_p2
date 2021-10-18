8bf3c177a83c47757a247cb927f66e83cebbbe4f
unknown <y8miao@gmail.com>
2021-10-18 11:00:48 -0400

    added annotations to application

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/PokemonTradingApplication.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/PokemonTradingApplication.java
@@ -2,7 +2,10 @@ package com.team5.PokemonTrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,6 +13,9 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan("com.team5.PokemonTrading") // alerts spring to look for components / stereotypes
+@EnableJpaRepositories("com.team5.PokemonTrading.repos") //tells spring where to find our repos so hibernate can do its work
+@EntityScan("com.team5.PokemonTrading.models") // tells spring where our entities are
 public class PokemonTradingApplication {
 
 	public static void main(String[] args) {
