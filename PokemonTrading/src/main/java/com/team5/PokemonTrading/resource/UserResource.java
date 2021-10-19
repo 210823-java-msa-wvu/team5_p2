3175a2f4ff2a96757e734134e69fbea461171b63
tungtt0806 <tung.tt0806@gmail.com>
2021-10-19 01:16:03 -0400

    Added deal processing controller

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/UserResource.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/UserResource.java
@@ -1,23 +1,34 @@
 package com.team5.PokemonTrading.resource;
 
 import com.team5.PokemonTrading.models.Deal;
+import com.team5.PokemonTrading.models.Transaction;
+import com.team5.PokemonTrading.models.User;
 import com.team5.PokemonTrading.services.DealServices;
+import com.team5.PokemonTrading.services.TransactionServices;
 import com.team5.PokemonTrading.services.UserServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserResource {
     private final UserServices userServices;
     private final DealServices dealServices;
+    private final TransactionServices transactionServices;
 
-    public UserResource(UserServices userServices, DealServices dealServices) {
+    public UserResource(UserServices userServices, DealServices dealServices, TransactionServices transactionServices) {
         this.userServices = userServices;
         this.dealServices = dealServices;
+        this.transactionServices = transactionServices;
     }
 
     @GetMapping("/sell/{id}")
@@ -32,5 +43,26 @@ public class UserResource {
         return new ResponseEntity<>(newDeal, HttpStatus.CREATED);
     }
 
+    @PostMapping("/buy/{id}")
+    public ResponseEntity<?> processDeal(@PathVariable("id") Integer id, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        Map<String, Cookie> cookieMap = new HashMap<>();
+        for (Cookie cookie : cookies) {
+            cookieMap.put(cookie.getName(), cookie);
+        }
 
+        Cookie getBalance = cookieMap.get("balance");
+        Deal currentDeal = dealServices.findById(id);
+        User currentUser = userServices.findUserById(id);
+        if (currentDeal.getPrice() < Float.parseFloat(getBalance.getValue())) {
+            Transaction newTransaction = new Transaction(currentDeal.getType(), currentUser, currentDeal.getSeller(), LocalDate.now(), currentDeal.getPrice(), currentDeal.getTradeFor(), currentDeal.getPokeId(), currentDeal.getDescription(), 1);
+            transactionServices.addTransaction(newTransaction);
+            dealServices.deleteDeal(id);
+            // Still waiting for Sergio create new updateBalance() method in order to use it to update the balance for seller and buyer
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+    }
 }
