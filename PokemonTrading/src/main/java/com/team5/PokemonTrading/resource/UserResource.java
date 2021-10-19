e560dc220191db8b308451888a5d992aa2344c21
unknown <y8miao@gmail.com>
2021-10-19 10:34:22 -0400

    added imports

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/UserResource.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/UserResource.java
@@ -1,5 +1,7 @@
 package com.team5.PokemonTrading.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team5.PokemonTrading.models.Deal;
 import com.team5.PokemonTrading.models.Transaction;
 import com.team5.PokemonTrading.models.User;
