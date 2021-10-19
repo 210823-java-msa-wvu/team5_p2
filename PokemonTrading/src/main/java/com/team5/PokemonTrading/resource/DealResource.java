14c70932af95b478faa3e09b3ac80aa61c1eb486
unknown <y8miao@gmail.com>
2021-10-19 18:05:00 -0400

    modified user posting new sales

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/DealResource.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/resource/DealResource.java
@@ -1,15 +1,21 @@
 package com.team5.PokemonTrading.resource;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team5.PokemonTrading.models.Deal;
 import com.team5.PokemonTrading.models.Pokemon;
 import com.team5.PokemonTrading.models.User;
 import com.team5.PokemonTrading.services.DealServices;
+import com.team5.PokemonTrading.services.PokemonServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/deal")
@@ -17,8 +23,13 @@ import java.util.List;
 public class DealResource {
 
     public final DealServices dealServices;
+    public final PokemonServices pokemonServices;
 
-    public DealResource (DealServices dealServices){this.dealServices = dealServices;}
+    @Autowired
+    public DealResource (DealServices dealServices, PokemonServices pokemonServices){
+        this.dealServices = dealServices;
+        this.pokemonServices = pokemonServices;
+    }
 
 
     //to get all deals in the market
@@ -30,9 +41,26 @@ public class DealResource {
 
 
     //to add new item to the market
-    @PostMapping("/sell")
-    public ResponseEntity<Deal> addDeal(@RequestBody Deal deal) {
-        Deal newDeal = dealServices.addDeal(deal);
+    @PostMapping(value = "/sell",consumes = "application/json")
+    public ResponseEntity<Deal> addDeal(@RequestBody Map<String,String> json,
+                                        @CookieValue("userinfo") String userinfo) throws JsonProcessingException {
+        ObjectMapper om = new ObjectMapper();
+        User u = om.readValue(userinfo,User.class);
+        Deal d = new Deal();
+        d.setDescription(json.get("description"));
+        d.setSeller(u);
+        d.setType(Integer.parseInt(json.get("type")));
+        d.setExpireDate(LocalDate.parse(json.get("expire_date")));
+        d.setPokeId(pokemonServices.findPokemonById(Integer.parseInt(json.get("pokeid"))));
+        //is a trade
+        if(d.getType()==3){
+            d.setTradeFor(pokemonServices.findPokemonById(Integer.parseInt(json.get("trade_for"))));
+        }
+        //is a buy now or auction
+        else{
+            d.setPrice(Float.parseFloat(json.get("price")));
+        }
+        Deal newDeal = dealServices.addDeal(d);
         return new ResponseEntity<>(newDeal, HttpStatus.CREATED);
     }
 
