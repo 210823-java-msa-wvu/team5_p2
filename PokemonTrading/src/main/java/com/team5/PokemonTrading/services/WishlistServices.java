84cd47368c1228055e09a6f3bfed1e4feabd17d9
DistractedBranches <damolajao@gmail.com>
2021-10-19 12:51:29 -0400

    Service viewWishlist added

--- PokemonTrading/src/main/java/com/team5/PokemonTrading/services/WishlistServices.java
+++ PokemonTrading/src/main/java/com/team5/PokemonTrading/services/WishlistServices.java
@@ -1,11 +1,14 @@
 package com.team5.PokemonTrading.services;
 
+import com.team5.PokemonTrading.models.User;
 import com.team5.PokemonTrading.models.Wishlist;
 import com.team5.PokemonTrading.repos.WishlistRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,6 +21,17 @@ public class WishlistServices {
     public Wishlist addWishlist(Wishlist wishlist){ return wishlistRepo.save(wishlist);}
 
     public void deleteWishlist(Integer id){ wishlistRepo.deleteById(id);}
+
+    public List<Wishlist> viewMyWishlist(User user){
+        List<Wishlist> wishlists = wishlistRepo.findAll();
+        List<Wishlist> myWishlists = new ArrayList<>();
+
+        for(Wishlist w : wishlists) {
+            if(w.getUserid().getId() == user.getId())
+                myWishlists.add(w);
+        }
+        return myWishlists;
+    }
 
 
 
