3aaee6a1212236332d130b7020175d472763183b
tungtt0806 <tung.tt0806@gmail.com>
2021-10-22 00:54:00 -0400

    Update user login & register features

--- pokemon-trading-angular/src/app/auth/components/login/login.component.ts
+++ pokemon-trading-angular/src/app/auth/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AlertService } from 'ngx-alerts';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { ProgressBarService } from 'src/app/shared/services/progress-bar.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,28 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, public progressBar: ProgressBarService, private alertService: AlertService) { }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
+    this.alertService.info('Checking User Info');
+    this.progressBar.startLoading();
 
     const loginObserver = {
-      next: x => console.log('User logged in'),
-      error: err => console.log(err)
+      next: x => {
+        this.progressBar.setSuccess();
+        console.log('User logged in');
+        this.alertService.success('Logged In');
+        this.progressBar.completeLoading();
+      },
+      error: err => {
+        this.progressBar.setError();
+        console.log(err);
+        this.alertService.danger('Unable to Login');
+        this.progressBar.completeLoading();
+      }
     };
 
     this.authService.login(f.value).subscribe(loginObserver);
--- pokemon-trading-angular/src/app/shared/shared.module.ts
+++ pokemon-trading-angular/src/app/shared/shared.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ColumnOneComponent } from './layouts/column-one/column-one.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { NgProgressModule } from 'ngx-progressbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AlertModule } from 'ngx-alerts';
 
 
 
@@ -14,7 +18,12 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgProgressModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    // Specify your library as an import (set timeout to -1 for unlimited timeout, the message can only be closed by the user clicking on it)
+    AlertModule.forRoot({ maxMessages: 5, timeout: 5000 })
   ],
   exports: [
     ColumnOneComponent
