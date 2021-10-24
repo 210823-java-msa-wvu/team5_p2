33103abea313c9c1aa997ae474882f9ba793f2fc
unknown <y8miao@gmail.com>
2021-10-24 18:28:45 -0400

    added user balance support, minor aesthetic changes

---
+++ pokemon-trading-angular/src/app/balance/balance.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BalanceService {
+
+  private url = environment.apiBaseUrl;
+  
+  constructor(private http: HttpClient) { }
+
+  public loadAmount(amount:number): Observable<void>{
+    const headers = {'content-type':"application/json"};
+    let body =     `{
+                      "amount":"${amount}"
+                    }`
+    //console.log(body);
+    return this.http.put<void>(`${this.url}/user/load`,body,{'headers':headers,withCredentials:true});
+  }
+}
