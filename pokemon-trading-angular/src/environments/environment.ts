f0616d07b5778efea9c726facb2f5d1ab547bc34
unknown <y8miao@gmail.com>
2021-10-26 19:22:31 -0400

    amazon s3

--- pokemon-trading-angular/src/environments/environment.ts
+++ pokemon-trading-angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://3.16.75.177:8080/t5p2',
+  apiBaseUrl: 'http://18.189.145.51:8080/t5p2',
   frontendBaseUrl: 'http://localhost:4200'
 };
 
