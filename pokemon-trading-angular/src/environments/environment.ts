817d87a3ad6b2f995b8ede2d2ffcbeddda192e71
unknown <y8miao@gmail.com>
2021-10-26 17:48:01 -0400

    added cors filter

--- pokemon-trading-angular/src/environments/environment.ts
+++ pokemon-trading-angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'http://3.16.75.177:8080/t5p2',
   frontendBaseUrl: 'http://localhost:4200'
 };
 
