41320b667ad36071e5306ec00ce7ffa2b3e36c32
unknown <y8miao@gmail.com>
2021-10-26 16:43:09 -0400

    changed getCookie to CookieService.get

--- pokemon-trading-angular/src/app/main-menu/main-menu.component.ts
+++ pokemon-trading-angular/src/app/main-menu/main-menu.component.ts
@@ -150,7 +150,7 @@ export class MainMenuComponent implements OnInit {
 
 
   public populateUser():void{
-    let cookie = this.getCookie("userinfo");
+    let cookie = this.cookieService.get("userinfo");
     this.currentUser = JSON.parse(JSON.parse(cookie));
   }
 
@@ -177,23 +177,6 @@ export class MainMenuComponent implements OnInit {
 
   public doNothing():void{
     
-  }
-
-
-
-  //helper function
-  // the cookie or `null`, if the key is not found.
-  private getCookie(name: string): string|null {
-    const nameLenPlus = (name.length + 1);
-    return document.cookie
-      .split(';')
-      .map(c => c.trim())
-      .filter(cookie => {
-        return cookie.substring(0, nameLenPlus) === `${name}=`;
-      })
-      .map(cookie => {
-        return decodeURIComponent(cookie.substring(nameLenPlus));
-      })[0] || null;
   }
 
   private deleteAllCookies() {
