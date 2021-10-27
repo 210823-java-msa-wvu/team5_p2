3cabbc8b235b14b4b18bd3fe418fd7074ad35242
tungtt0806 <tung.tt0806@gmail.com>
2021-10-26 22:27:22 -0400

    Resolve overwritten

--- pokemon-trading-angular/src/environments/environment.ts
+++ pokemon-trading-angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://18.189.145.51:8080/t5p2',
+  apiBaseUrl: 'http://localhost:8080',
   frontendBaseUrl: 'http://localhost:4200'
 };
 
